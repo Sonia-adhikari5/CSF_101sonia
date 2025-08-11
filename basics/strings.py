ac0ac6dc98fa831d020dd4dba784f2c8a0803286
Sonia-adhikari5 <02250371.cst@rub.edu.bt>
2025-08-12 00:36:58 +0600

    exercise 2.3.1

---
+++ basics/strings.py
@@ -0,0 +1,10 @@
+name = "sonia"
+print (name)
+greeting_f = "hello," + name + "!"
+print (greeting_f)
+greeting = f"hello, {name} !"
+print (greeting)
+name_length = len(name)
+print(name_length)
+
+
---
+++ data structures/lists.py/lists.py
@@ -0,0 +1,16 @@
+fruits = ["apple", "banana", "cherry"]
+print(fruits)
+fruits.append("date")
+print(fruits)
+second_fruit = fruits[1]
+print(second_fruit)
+name = "sonia adhikari" 
+age = 18 
+height = 167 
+is_student = True 
+person_info = {"name": name,"age": age, "height": height, "is_student": is_student}
+print(person_info)
+person_info["favorite_color"] = "blue" 
+print(person_info)
+print(person_info["weight"])
+print(f"Error: {e}")
